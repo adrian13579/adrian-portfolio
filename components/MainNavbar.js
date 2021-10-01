e96a1b7d03421b9732aad12b3ba04472b5687709
Adrian Portales <adrianportales135@gmail.com>
2021-10-01 18:16:16 -0400

    Create main page layout and style

--- components/MainNavbar.js
+++ components/MainNavbar.js
@@ -1,28 +1,22 @@
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap"
+import { Navbar, Container, Nav } from "react-bootstrap"
 
 
-export default function MainNavbar(){
+export default function MainNavbar({styles}){
   return (
-    <>
-      <Navbar bg="light" expand="lg">
+    <div>
+      <Navbar className={styles} fixed="top" >
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Home</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
+            <Nav className="">
+              <Nav.Link className="me-3" href="#home">Work</Nav.Link>
+              <Nav.Link className="me-3" href="#link">About</Nav.Link>
+              <Nav.Link className="me-3" href="#link">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    </>
+    </div>
   )
 }
